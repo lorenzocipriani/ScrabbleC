950522c2449caeb6fe8af3e1e594dd4f44486e01
AlessandroSpallina <alessandrospallina1@gmail.com>
2015-09-11 22:12:22 +0200

    aggiunto main (manca ultima funzione e finito davvero), readme, ipcutils.h

---
+++ src/main/gameboard/main.c
@@ -0,0 +1,69 @@
+/****************************************************************************
+* Copyright © 2015 Alessandro Spallina
+* email: alessandrospallina1@gmail.com
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "gameboard.h"
+#include "tiles.h"
+#include "ipcutils.h"
+
+void execute_command(const char *command)
+{
+	//domani finisco, c'è da fare l'ultima cazzata :)
+	
+	
+}
+
+int main()
+{
+	int ret, message_to_receive=0, running=1;
+	struct stringMessage message;
+	
+		ret=msgget((key_t)QUEUEID, 0666 | IPC_CREAT);
+		if(ret==-1) {
+			fprintf(stderr,"%s\n",strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		
+		while(running) {
+			if(msgrcv(ret, (void *)&message, BUFSIZ, 
+			message_to_receive, 0)==-1) {
+				fprintf(stderr, "%s\n", strerror(errno));
+				exit(EXIT_FAILURE);
+			}
+			
+			printf("Ricevuto comando [%s]\n", message.string);
+			
+			execute_command(message.string);
+			
+			
+			if(strncmp(message.string, "stop", 4)==0)
+				running=0;
+				
+			if(msgctl(ret, IPC_RMID, 0)==-1) {
+				fprintf(stderr, "%s\n", strerror(errno));
+				exit(EXIT_FAILURE);
+			}	
+		}
+		
+	exit(EXIT_SUCCESS);
+}
